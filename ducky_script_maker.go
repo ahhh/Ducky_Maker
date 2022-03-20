924ee17a09b984a10513bc09c7a54a21e1b0df85
Dan Borges <ahhh.db@gmail.com>
2022-03-20 13:21:13 -0400

    Add files via upload

---
+++ ducky_script_maker.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+  //"os"
+  "flag"
+  "io/ioutil"
+  "os"
+  "bufio"
+  "strings" 
+  "fmt"
+)
+
+var (
+
+  outFile  = flag.String("outFile", "", "File to output new ducky script")
+  inFile = flag.String("inFile", "", "File to turn into a ducky script")
+  verbose  = flag.Bool("verbose", false, "verbosly send messages to the console")
+
+)
+
+func paramCheck() bool {
+  canRun := true
+  // Make sure outFile
+  if (*outFile != "") {
+    if *verbose == true {
+      fmt.Println("outFile has values set to " + string(*outFile))
+    }
+  } else {
+    fmt.Println("No outFile provided!")
+    canRun = false
+  }
+  // Make sure inFile is set
+  if (*inFile != "") {
+    if *verbose == true {
+      fmt.Println("inFile has values set to " + string(*outFile))
+    }
+  } else {
+    fmt.Println("No inFile provided!")
+    canRun = false
+  }
+
+  if !canRun {
+    fmt.Println("Missing mandatory paramaters. use -h for the help menu.")
+    return false
+  } else {
+    return true
+  }
+
+}
+
+
+func main() {
+
+  flag.Parse()
+  inFileFlag := flag.Lookup("inFile")
+  outFileFlag := flag.Lookup("outFile")
+
+  shouldRun := paramCheck()
+  if !shouldRun {
+    if *verbose == true {
+      fmt.Println("Failed mandatory flag checks, plz set required flags!!")
+    }
+  }
+
+  fileData := ReadFile(inFileFlag.Value.String())
+  new_script := Format_lines(fileData)
+  new_file := strings.Join(new_script[:],"\n")
+  err := WriteFile(new_file, outFileFlag.Value.String())
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+}
+
+
+func Format_lines(work []string) []string{
+  //fmt.Printf("dat work: %s\n", work)
+  var newHotness []string
+  for _, line := range work {
+    //fmt.Printf("sentance %d: %s\n", i, string(line))
+    newHotness = append(newHotness,"STRING " + string(line))
+
+  }
+  return newHotness
+
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadFile(path string) []string {
+  file, err := os.Open(path)
+  if err != nil {
+    fmt.Println(err.Error())
+    return nil
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines
+}
+
+
+func WriteFile(text, outFile string) (error) {
+  //Check if log file exists to create or append
+  if Exists(outFile) {
+    //Write the lines to the file
+    f, err := os.OpenFile(outFile,
+       os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+      fmt.Println(err.Error())
+      return err
+    }
+    defer f.Close()
+    if _, err := f.WriteString(text); err != nil {
+      fmt.Println(err.Error())
+      return err
+    }  
+  } else {
+    //Create file and write first lines
+  err := ioutil.WriteFile(outFile, []byte(text), 0700)
+  if err != nil {
+  	  fmt.Println(err.Error())
+      return err
+    }
+  }
+  return nil
+}
+
+
+func Exists(path string) bool {
+  // Run stat on a file
+  _, err := os.Stat(path)
+  // If it runs fine the file exists
+  if err == nil {
+    return true
+  }
+  // If stat fails then the file does not exist
+  if *verbose == true { fmt.Println("Creating new file as " + err.Error()) }
+  return false
+}
+
